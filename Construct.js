7b0401acfbe269bf255f3ab2814bbeaa24dbc640
kdrdr3amz <47705734+JoseOr1j@users.noreply.github.com>
2019-11-14 22:27:52 -0600

    Recent push
    Files after hosted website

---
+++ Construct.js
@@ -0,0 +1,34 @@
+//alert("Welcome to my Website! You are now Connected!");
+/*f (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+} else {
+    txt = "You pressed Cancel!";
+}*/
+
+/*
+Have to figure out the order of the sub pages
+Home, Characters and Abilities is what we 
+have now, Home will stay, but the others have to be changed
+I am unsure what would be interisting enough to 
+put as one of the sub topics and such
+here are some ideas to spit out
+
+not neceszaarlily in that order
+Home, Concept Art, Endings
+Home will have the basic info about the game,
+characters, and their looks
+Concept Art will have the art of the game, videos
+and images
+Endings will have details about the multiple endings 
+of Nier Automata
+i think that is fine
+
+learn to impliment more javascript to the website
+how to add videos
+how to add more images
+how to change the apprance of the two subpages none Home
+fix grammar, info, overall looks,
+re host
+eventually use a database for actual info recieve
+put on githib/resume
+*/
